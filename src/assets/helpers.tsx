80370cf377be2badfbbc0850f4834376b946bca4
Rocio Alcala <rro.alcala@gmail.com>
2024-08-20 20:13:51 -0300

    background on navbar bar on scroll

--- src/assets/helpers.tsx
+++ src/assets/helpers.tsx
@@ -1,3 +1,10 @@
+import clsx, { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function isRunningOnClient() {
   return typeof window !== "undefined";
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(...inputs));
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,30 @@
 import { BiMenu, BiX } from "react-icons/bi";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavBarList from "./NavBarList";
+import { cn } from "../assets/helpers";
 
 export default function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isScroll, setIsScroll] = useState(false);
+
+  useEffect(() => {
+    // check initial state of scroll to style NavBar
+    if (window.scrollY > 50) setIsScroll(true);
+
+    // event listener on scroll and set state to style navbar
+    function handleScroll() {
+      if (window.scrollY > 50) setIsScroll(true);
+      else {
+        setIsScroll(false);
+      }
+    }
+    window.addEventListener("scroll", handleScroll);
+
+    // clean event listener and observer
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   function handleMenuToggle() {
     setIsMenuOpen(!isMenuOpen);
@@ -18,16 +39,23 @@ export default function NavBar() {
   return (
     <>
       <nav>
+        {/* background on scroll div */}
+        <div
+          className={cn(
+            `fixed left-0 top-[-100px] z-[1000] flex h-20 w-full justify-end bg-[#0e100f]/80 p-5 text-[#ffffff] opacity-0 shadow-lg transition-all duration-700 ease-out`,
+            isScroll && "top-0 opacity-100"
+          )}
+        ></div>
         {/* desktop navbar */}
-        <div className="fixed right-0 top-0 z-50 hidden h-20 w-full animate-fade-out-top items-center justify-end bg-transparent p-6 transition-all duration-500 md:visible md:flex">
-          <ul className="flex text-xl gap-20 font-bold text-white px-5 ">
+        <div className="fixed right-0 top-0 z-[1050] hidden h-20 w-full animate-fade-out-top items-center justify-end bg-transparent p-6 transition-all duration-500 md:visible md:flex">
+          <ul className="flex text-xl gap-20 font-bold text-[#ffffff] px-5 ">
             <NavBarList />
           </ul>
         </div>
         {/* toggle mobile navbar */}
         <div
-          className={`fixed right-0 top-0 z-50 text-white flex h-20 w-full animate-fade-out-top items-center justify-end ${
-            isMenuOpen ? "bg-black" : ""
+          className={`fixed right-0 top-0 z-[1050] text-[#ffffff] flex h-20 w-full animate-fade-out-top items-center justify-end ${
+            isMenuOpen ? "bg-[#0e100f]" : ""
           } p-2 pr-5 transition-all duration-500 md:hidden md:opacity-0`}
         >
           {isMenuOpen ? (
