1835fe2f5ba3b0735623ed6eb5e3f9d2e56e645a
Rocio Alcala <rro.alcala@gmail.com>
2024-08-24 16:53:33 -0300

    contact page

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { cn } from "../assets/helpers";
+import ContactSection from "../components/ContactSection";
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div
+      style={{
+        backgroundImage: "url('/background-2.jpg')",
+        backgroundPosition: "75% 25%"
+      }}
+      className={cn(
+        "flex-1 flex flex-col md:flex-row p-6 sm:p-10 md:p-15 bg-cover animate-fade-in",
+        "pt-28 sm:pt-28 md:pt-32"
+      )}
+    >
+      <div className="md:w-[60%]">Mapa</div>
+      <ContactSection />
+    </div>
+  );
 }
